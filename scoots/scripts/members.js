61ef0cf0e4c4fb685e481d74645df7856cb1f102
deivezera <dzabroski@gmail.com>
2023-12-12 20:44:07 -0300

    cozumel home page start

---
+++ scoots/scripts/members.js
@@ -0,0 +1,67 @@
+const membersURL = "https://deivezera.github.io/wdd230/chamber/data/members.json";
+const membersList = document.getElementById("membersList");
+async function getMembers() {
+  const response = await fetch(membersURL);
+  const data = await response.json();
+  displayMembers(data.members);
+}
+
+function displayMembers(data){
+  data.forEach((member) => {
+
+      let card = document.createElement('div');
+
+      let logo = document.createElement('img');
+      logo.setAttribute("src", member.image);
+      logo.setAttribute("alt", member.name);
+      logo.setAttribute("width", "300px");
+      logo.setAttribute("height", "182px");
+
+      card.appendChild(logo)
+
+      let name = document.createElement('p');
+      name.innerHTML = member.name
+      name.classList.add("name");
+      card.appendChild(name);
+
+      let address = document.createElement('p');
+      address.innerHTML = member.address
+      address.classList.add("address");
+      card.appendChild(address);
+
+      let phone = document.createElement('p');
+      phone.innerHTML = member.phone;
+      phone.classList.add("phone");
+      card.appendChild(phone);
+
+      let website = document.createElement('a');
+      website.innerHTML = member.website;
+      website.setAttribute("href" , member.website);
+      website.setAttribute("target", "_blank");
+      website.classList.add("website");
+      card.appendChild(website);
+
+
+      membersList.appendChild(card);
+  }); 
+}
+
+getMembers();
+
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#membersList");
+
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("grid_members");
+	display.classList.remove("list_members");
+});
+
+listbutton.addEventListener("click", showList); 
+
+function showList() {
+	display.classList.add("list_members");
+	display.classList.remove("grid_members");
+}
---
+++ scoots/scripts/weather.js
@@ -0,0 +1,50 @@
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?`;
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?`
+const weatherId = document.getElementById("weatherId");
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const getWeather = async (url, lat, lon) => {
+  const response = await fetch(url + `lat=${lat}&lon=${lon}&appid=478e9aabd0ecdd85f02732bb1bd8416a&units=imperial`);
+  const data = await response.json();
+  
+  var iconurl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+  
+  let logo = document.createElement('img');
+  logo.setAttribute("src", iconurl)
+  logo.setAttribute("alt", data.weather[0].description)
+  
+  weatherId.appendChild(logo)
+  let weatherText = document.createElement('p');
+  weatherText.innerHTML = `${data.main.temp.toFixed(0)}ºF - ${data.weather[0].main}`
+  weatherId.appendChild(weatherText)
+
+  let weatherDescription = document.createElement('p');
+  weatherDescription.innerHTML = `${capitalizeFirstLetter(data.weather[0].description)}`
+  weatherId.appendChild(weatherDescription)
+
+  getForecast(forecastUrl, lat, lon)
+}
+
+const getForecast = async (url, lat, lon)  => {
+  const response = await fetch(url + `lat=${lat}&lon=${lon}&appid=478e9aabd0ecdd85f02732bb1bd8416a&units=imperial`);
+  const data = await response.json();
+  const threeDayForecast = data.list.filter((item, index) => [8,16,24].includes(index))
+  threeDayForecast.forEach((item, index) => {
+    let forecastText = document.createElement('p');
+    forecastText.innerHTML = `${new Date(item.dt_txt).toLocaleString().split(',')[0]} ${item.main.temp.toFixed(0)}ºF - ${item.weather[0].main}`
+    weatherId.appendChild(forecastText)
+  })
+}
+const successCallback = (position) => {
+  getWeather(weatherUrl, position.coords.latitude, position.coords.longitude)
+};
+
+const errorCallback = (error) => {};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+
+
